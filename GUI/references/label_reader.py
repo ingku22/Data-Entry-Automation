ca7c85ee36694f4dab4237227154fcfaab8741d1
IngKung <2b02.2214605.ingkungsuwun@gmail.com>
2024-04-10 01:33:07 +0800

    put label templates into references

--- GUI/references/label_reader.py
+++ GUI/references/label_reader.py
@@ -4,6 +4,9 @@ with open('labels template.json') as json_string:
     data_dict = json.load(json_string)
 
 
-print(data_dict['image 1']['crop 1.1'])
+details = data_dict['image 1']['crop 1.1']['details']
+
+print(details.keys())
+
 
 
