91826e6594979f8fa2a333ab5cb0edbec87ba22d
javGithubSchool <JAVIERLING24.22@ichat.sp.edu.sg>
2024-04-15 15:03:30 +0800

    settings popout

--- GUI/app_p1/pages/backend/popout.py
+++ GUI/app_p1/pages/backend/popout.py
@@ -1,10 +1,9 @@
 import tkinter as tk
 
 class settings_popout():
-    def __init__(self, parent, main_app):
+    def __init__(self, parent):
         self.parent = parent # to get page class functions
-        self.main_app = main_app # tkinter window located in the main page_pagination
-        self.window = tk.Toplevel(parent.root)
+        self.window = tk.Toplevel(parent)
 
         # Settings variables
 
@@ -13,13 +12,10 @@ class settings_popout():
         # windows testing
         self.entry_label = tk.Label(self.window, text='Test:')
         self.entry = tk.Entry(self.window, width=9)
+        print("As")
 
     def get_app_data(self):
         pass
 
     def get_settings_info(self):
         return self.entry.get()
-
-
-
-
